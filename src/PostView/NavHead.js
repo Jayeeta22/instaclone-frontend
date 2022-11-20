7ef1f6652a8065d7654fec40e7e218543777f7b4
Jayeeta22 <117907901+Jayeeta22@users.noreply.github.com>
2022-11-20 23:34:13 +0530

    image changed

--- src/PostView/NavHead.js
+++ src/PostView/NavHead.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export default function NavHead(){
     return(
         <div className="Nav">
-            <p className="logo"><img src={require('../Svg/instagram.png').default} alt=""/>Instaclone</p>
-            <div className="Camicon"><Link to = "/uploadview" ><img className="Cam" src={require('../Svg/camera.jpg').default} alt=""/></Link></div>
+            <p className="logo"><img src={require('../Svg/circle-oval-svgrepo-com.svg').default} alt=""/>Instaclone</p>
+            <div className="Camicon"><Link to = "/uploadview" ><img className="Cam" src={require('../Svg/camera-svgrepo-com.svg').default} alt=""/></Link></div>
         </div>
     )
 }
